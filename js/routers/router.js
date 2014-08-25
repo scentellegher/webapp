ddd6bfe66639d9301ec70a73c0717427d435ce6a
scentellegher <centellegher.simone@gmail.com>
2014-08-25 13:11:48 +0200

    placeView

--- js/routers/router.js
+++ js/routers/router.js
@@ -6,29 +6,21 @@ define([
   'views/quoteView',
   'views/recipeView',
   'views/placeView'
-  // 'views/projects/ProjectsView',
-  // 'views/contributors/ContributorsView',
-  // 'views/footer/FooterView'
-], function($, _, Backbone, QuoteView, RecipeView, PlaceView){//, HomeView, ProjectsView, ContributorsView, FooterView) {
+], function($, _, Backbone, QuoteView, RecipeView, PlaceView){
   
   var AppRouter = Backbone.Router.extend({
     routes: {
       // Define some URL routes
       '' : 'home'
-      // 'projects': 'showProjects',
-      // 'users': 'showContributors',
-      
-      // Default
-      //'*actions': 'defaultAction'
     }
   });
   
   var initialize = function(){
-    console.log("entrato nel router")
+    
     var app_router = new AppRouter;
     
     app_router.on('route:home', function(){
-      console.log("entro home");
+    
       var quoteView = new QuoteView();
       quoteView.render();
       var recipeView = new RecipeView();
@@ -36,31 +28,7 @@ define([
       var placeView = new PlaceView();
       placeView.render();
 
-        // Call render on the module we loaded in via the dependency array
-        // var projectsView = new ProjectsView();
-        // projectsView.render();
-
     });
-
-    // app_router.on('route:showContributors', function () {
-    
-    //     // Like above, call render but know that this view has nested sub views which 
-    //     // handle loading and displaying data from the GitHub API  
-    //     var contributorsView = new ContributorsView();
-    // });
-
-    // app_router.on('route:defaultAction', function (actions) {
-     
-    //    // We have no matching route, lets display the home page 
-    //     var homeView = new HomeView();
-    //     homeView.render();
-    // });
-
-    // Unlike the above, we don't call render on this view as it will handle
-    // the render call internally after it loads data. Further more we load it
-    // outside of an on-route function to have it loaded no matter which page is
-    // loaded initially.
-    // var footerView = new FooterView();
 
     Backbone.history.start();
   };
--- js/views/placeView.js
+++ js/views/placeView.js
@@ -3,27 +3,65 @@ define([
   'underscore',
   'backbone',
   'collections/placeList',
+  'gmaps',
   'text!templates/place/placeTemplate.html'
-], function($, _, Backbone, PlaceList, placeTemplate){
+], function($, _, Backbone, PlaceList, gmaps, placeTemplate){
   var PlaceView = Backbone.View.extend({
-    el: '.gmap',
-    render: function(){
+    el: '.mappa',
+    initialize: function(){
+      this.places = new PlaceList();
+      
+      var template = _.template(placeTemplate);
+      this.$el.html(template);  
+    },
+    render: function(places){
       var that = this; 
-      var places = new PlaceList();
-      places.fetch({
+      this.places.fetch({
         success : function (places) {
+          //set position
+          var myPos = new gmaps.LatLng(46.069744, 11.118982);
+          console.log(myPos);
+          //create list of points
+          var points = [];
+          //for each place
+          _.each(places.models, function(place){
+              console.log(place.get('name'));
+              console.log(place.get('latitude'));
+              console.log(place.get('longitude'));
+            points.push({
+              
+              title: place.get('name'),
+              position: new gmaps.LatLng(place.get('latitude'), place.get('longitude')),
+              icon: 'img/jogging.png' 
+            });  
+          });
+          console.log("lunghezza="+points.length);
+
+          // define map options
+          var mapOptions = {
+            center: myPos,
+            zoom: 8
+          };
+
+          console.log(mapOptions);
+          // init map
           
-          // console.log("places");
-          // console.log(places);
-          // console.log("places.models");
-          // console.log(places.models);
-          // console.log("places.toJSON");
-          // console.log(places.toJSON());
-          var template = _.template(placeTemplate, {places: places.models});
-          that.$el.html(template);    
+          console.log("esiste? "+ document.getElementById("map-canvas"));
+          var map = new gmaps.Map(document.getElementById("map-canvas"), mapOptions);
+          
+          // define markers
+          var markers = [];
+          _.each(points, function(point){
+            markers.push(new gmaps.Marker(point));
+          });
+          console.log(markers);
+          // add markers to map
+          _.each(markers, function(marker){
+            marker.setMap(map);
+          });
+              
         }
       });
-      
     }
   });
   // Our module now returns our view
